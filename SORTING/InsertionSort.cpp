2016dc53d40c2e9a2cc4248ad5527a70a1f80b1a
handevyanktesh <handevyanktesh99@gmail.com>
2026-02-13 23:36:15 +0530

    Update InsertionSort.cpp

--- SORTING/InsertionSort.cpp
+++ SORTING/InsertionSort.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int arr[] = {4,3,2,5,6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout<<endl;
+
+    for (int i = 0; i < n; i++)
+    { 
+        int j = i;
+        while (j>=1)
+        {
+            if(arr[j]>=arr[j-1]) break;
+            else swap(arr[j],arr[j-1]);
+            j--;
+        }
+    }
     
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout<<endl;
 }
