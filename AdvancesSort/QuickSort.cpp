6d79ac7ef9a104051dd5a1c4ce1d4743e66313a1
handevyanktesh <handevyanktesh99@gmail.com>
2026-03-07 15:10:11 +0530

    Update QuickSort.cpp

--- AdvancesSort/QuickSort.cpp
+++ AdvancesSort/QuickSort.cpp
@@ -1,3 +1,43 @@
+#include <iostream>
+using namespace std;
+int partition(int arr[],int si, int ei){
+    int pivotELement = arr[si];
+    int count = 0;
+    for(int i = si+1; i<=ei; i++){
+        if(arr[i]<pivotELement) count++;
+    }
+    int pivotIdx = count + si;
+    swap(arr[si],arr[pivotIdx]);
+    int i = si;
+    int j = ei;
+    while(i<pivotIdx && j>pivotIdx){
+        if(arr[i]<= pivotELement) i++;
+        if(arr[j]>pivotELement) j--;
+        else if (arr[i]>pivotELement && arr[j]<pivotELement){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }  
+    }return pivotIdx;
+}
 
+void quicksort(int arr[],int si,int ei){
+    if(si>=ei) return;
+    int pi = partition(arr,si,ei);
+    quicksort(arr,si,pi-1);
+    quicksort(arr,pi+1,ei);
+}
+int main(){
+    int arr[] = {5,1,3,2,4,6,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+    quicksort(arr,0,n-1);
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+}
 
 
