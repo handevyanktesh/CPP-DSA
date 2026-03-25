f0e60e87ce435627ae7473a61f19d61c3f609264
handevyanktesh <handevyanktesh99@gmail.com>
2026-03-25 23:29:14 +0530

    Update BInaryToDecimal.cpp

--- BITWISE/BInaryToDecimal.cpp
+++ BITWISE/BInaryToDecimal.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int Binary_to_Descimal(string &binary){
-    int res = 0;
-    int n = binary.size();
-    for(int i=n-1; i>=0;i--){
-        char ch = binary[i];
-        int num = ch - '0';
-        res = res + num*(1<<(n-i - 1));
+// int Binary_to_Descimal(string &binary){
+//     int res = 0;
+//     int n = binary.size();
+//     for(int i=n-1; i>=0;i--){
+//         char ch = binary[i];
+//         int num = ch - '0';
+//         res = res + num*(1<<(n-i - 1));
+//     }
+//     return res;}
+
+    //Method - 2
+    int Binary_to_Descimal(string &x) {
+    int len = x.length();
+    int p2 = 1;   // power of 2
+    int num = 0;
+
+    for(int i = len - 1; i >= 0; i--) {
+        if(x[i] == '1') {
+            num = num + p2;
+        }
+        p2 = p2 * 2;
     }
-    return res;
-}
+
+    return num;
+    }
+
+
 int main(){
         string str = "1101";
         cout<<Binary_to_Descimal(str);
