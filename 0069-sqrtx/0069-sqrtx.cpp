2ab5847338d5011ef1aeb0fe2381f654c9ebf3e7
handevyanktesh <handevyanktesh99@gmail.com>
2026-07-11 11:56:53 +0530

    0069-sqrtx 4 ms (14.57%) 07-11-2026 .cpp

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int l = 1;
+        int r = x;
+        int ans = 0;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            long long sq = 1LL * mid * mid;
+
+            if (sq == x)
+                return mid;
+            else if (sq < x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
