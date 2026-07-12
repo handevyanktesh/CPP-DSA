d95da122dbbaddece8d8b680c9e19846c4aa4a80
handevyanktesh <handevyanktesh99@gmail.com>
2026-07-12 10:21:59 +0530

    0198-house-robber 0 ms (100%) 07-12-2026 .cpp

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-
-        int n = nums.size();
-        if(n==1) return nums[0];
         
-        vector<int>dp(n+1,0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        int prevRob = 0;
+        int maxRob = 0;
 
-        for(int i=2; i<n; i++){
-            dp[i] = max(dp[i-1],nums[i]+dp[i-2]);
+        for(int p: nums){
+            int temp = max(maxRob,(p+prevRob));
+            prevRob = maxRob;
+            maxRob = temp;
         }
-        return dp[n-1];
+        return maxRob;
     }
 };
