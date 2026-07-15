44f6becc866a3ea117f1954140374e1485d000e3
handevyanktesh <handevyanktesh99@gmail.com>
2026-07-15 23:08:48 +0530

    0316-remove-duplicate-letters 0 ms (100%) 07-15-2026 .cpp

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+        vector<int> freq(26, 0);
+        vector<bool> inStack(26, false);
+        stack<char> st;
+
+        for (char ch : s)
+            freq[ch - 'a']++;
+
+        for (char ch : s) {
+
+            freq[ch - 'a']--;
+
+            if (inStack[ch - 'a'])
+                continue;
+
+            while (!st.empty() &&
+                   ch < st.top() &&
+                   freq[st.top() - 'a'] > 0) {
+
+                inStack[st.top() - 'a'] = false;
+                st.pop();
+            }
+
+            st.push(ch);
+            inStack[ch - 'a'] = true;
+        }
+
+        string ans = "";
+
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
