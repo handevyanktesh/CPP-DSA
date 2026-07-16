3ae01987582b9e62ed38293396b16a90635561df
handevyanktesh <handevyanktesh99@gmail.com>
2026-07-16 18:17:42 +0530

    3658-gcd-of-odd-and-even-sums 0 ms (100%) 07-16-2026 .cpp

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        for(int i=1; i<=2*n;i++){
+            if(i%2 == 0) sumEven += i;
+            else sumOdd += i;
+        }
+        return gcd(sumOdd,sumEven);
     }
 };
