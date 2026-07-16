583f01958e19b1eb7be68189d69f16bd8dd21de1
handevyanktesh <handevyanktesh99@gmail.com>
2026-07-16 18:02:31 +0530

    0645-set-mismatch 28 ms (22.11%) 07-16-2026 .cpp

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        unordered_map<int,int>mp;
+
+        for(int x:nums){
+            mp[x]++;
+        }
+
+        int ans = 0;
+        for(auto x:mp){
+            if(x.second>1){ 
+                ans = x.first;
+                break;
+            }
+        }
+
+        int missing = 0;
+        for(int i = 1; i <= nums.size(); i++){
+            if(mp.find(i) == mp.end()){
+                missing = i;
+                break;
+            }
+        }
+
+        vector<int>v;
+        v.push_back(ans);
+        v.push_back(missing);
+
+        return v;
+    }
+};
