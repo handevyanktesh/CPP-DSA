4ff8cba436b5e55cba1ef7573beadf7eaf5fb64c
handevyanktesh <handevyanktesh99@gmail.com>
2026-08-04 21:35:50 +0530

    0350-intersection-of-two-arrays-ii 4 ms (16.85%) 08-04-2026 .cpp

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+        unordered_map<int,int>mp;
+
+        for(int n:nums1){
+            mp[n]++;
+        }
+
+        vector<int>ans;
+
+        for(int n:nums2){
+            if(mp[n]>0){
+            ans.push_back(n);
+            mp[n]--;
+            }
+        }
+        return ans;
+    }
+};
