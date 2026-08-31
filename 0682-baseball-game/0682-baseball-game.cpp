d30c23b4ef2c104f36fb3e9b2168ee219870b17e
handevyanktesh <handevyanktesh99@gmail.com>
2026-08-31 18:58:43 +0530

    0682-baseball-game 0 ms (100%) 08-31-2026 .cpp

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+
+        vector<int> scores;
+
+        for (string op : operations) {
+
+            if (op == "C") {
+                scores.pop_back();
+            }
+            else if (op == "D") {
+                scores.push_back(2 * scores.back());
+            }
+            else if (op == "+") {
+                int n = scores.size();
+                scores.push_back(scores[n-1] + scores[n-2]);
+            }
+            else {
+                scores.push_back(stoi(op));
+            }
+        }
+
+        int total = 0;
+
+        for (int x : scores)
+            total += x;
+
+        return total;
+    }
+};
